03b04617fd81c29def013d73a30df482cbca6ffd
scottrbtrsn <scott.peter.robertson@gmail.com>
2018-12-27 17:57:54 -0600

    Updates

---
+++ precache-manifest.4db8a78ea39edb6272f60049c5d25de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scottrbtsn/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "96aa5dd2cf522cc3a703",
+    "url": "/scottrbtsn/static/js/runtime~main.96aa5dd2.js"
+  },
+  {
+    "revision": "aced5273b099009d76b5",
+    "url": "/scottrbtsn/static/js/main.aced5273.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/scottrbtsn/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "aced5273b099009d76b5",
+    "url": "/scottrbtsn/static/css/main.e876bd1e.chunk.css"
+  },
+  {
+    "revision": "4488ec0319c793abdbbc1a8426ed808e",
+    "url": "/scottrbtsn/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c7e3982af612fb0131ad732c53d86de.js"
+  "/scottrbtsn/precache-manifest.4db8a78ea39edb6272f60049c5d25de3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/scottrbtsn/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
